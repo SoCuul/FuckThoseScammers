0b5fe2b78b559c749ae70e99876455fafb2620f1
SoCuul <socuul@outlook.com>
2022-07-20 16:22:47 -0700

    Create hype-join.ga.js

---
+++ spammers/hype-join.ga.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+export default async ({ email, password }) => {
+    try {
+        //Make request
+        const req = await axios({
+            method: 'post',
+            url: 'https://hype-join.ga/login/',
+            data: {
+                email: email,
+                password: password
+            }
+        })
+
+        //Parse response
+        if (req?.status === 200) return {
+            error: false,
+            email: email,
+            password: password
+        }
+        else return {
+            error: true,
+            email: email,
+            password: password
+        }
+    }
+    catch (error) {
+        return {
+            error: true,
+            email: email,
+            password: password
+        }
+    }
+}
